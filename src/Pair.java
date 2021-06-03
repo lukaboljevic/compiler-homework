74ee3c795942e312d7ff7a9f45370cdd3274dc0d
Luka Boljevic <lukaboljevic.boljevic@gmail.com>
2021-06-03 11:14:26 +0200

    Added missing Pair class

---
+++ src/Pair.java
@@ -0,0 +1,11 @@
+public class Pair {
+
+    // Defines a pair for the symbol table
+    String type;
+    Expression expression;
+
+    public Pair(String type, Expression expression){
+        this.type = type;
+        this.expression = expression;
+    }
+}
